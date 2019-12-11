1844f3505d6bc18400c95a2537ffcc4605790484
Predrag <47817739+PredragMitic@users.noreply.github.com>
2019-12-11 19:49:31 +0100

    Izdvojene funkcije
    U ovom fajlu sam izdvoji funkcije iz main-a da bi kod bio pregledniji

---
+++ Code/func.c
@@ -0,0 +1,64 @@
+#include "func.h"
+#include <stdlib.h>
+#include <GL/glut.h>
+
+void draw_tarrget(int h, int rot, int trn){
+    glPushMatrix();
+    glRotatef(rot,0,1,0);
+    glTranslatef(0,h,trn);
+    
+    float r = 0.5;
+    
+    glBegin(GL_POLYGON);
+        glColor3f(1,1,1);
+        glNormal3f(0,0,-1);
+        glVertex3f(r,-r,0);
+        glVertex3f(-r,-r,0);
+        glVertex3f(-r,r,0);
+        glVertex3f(r,r,0);
+    glEnd();
+
+    r -= 0.05;
+
+    glTranslatef(0,0,-0.1);
+    glColor3f(0,0,1);
+    glutSolidCone(r, 0, 40, 0);
+    glTranslatef(0,0,-0.2);
+    glColor3f(1,0,0);
+    glutSolidCone(r/2, 0, 40, 0);
+    glColor3f(0,0,0);
+    glutSolidSphere(0.05,5,5);
+    
+    glTranslatef(0,0,0.3);
+
+    glBegin(GL_POLYGON);
+        glColor3f(0,0,0);
+        glNormal3f(0,0,1);
+        glVertex3f(r,-r,0);
+        glVertex3f(r-0.1,-r,0);
+        glVertex3f(r-0.1,-h,0);
+        glVertex3f(r,-h,0);
+    glEnd();
+    
+     glBegin(GL_POLYGON);
+        glColor3f(0,0,0);
+        glNormal3f(0,0,1);
+        glVertex3f(-r,-r,0);
+        glVertex3f(-r+0.1,-r,0);
+        glVertex3f(-r+0.1,-h,0);
+        glVertex3f(-r,-h,0);
+    glEnd();
+
+    glPopMatrix();
+}
+
+void fire(float bullet_trn)
+{
+    glPushMatrix();
+    glTranslatef(0,1.6,bullet_trn);
+    
+    glColor3f(1,0,0);
+    glutSolidSphere(0.01,10,10);
+
+    glPopMatrix();
+}
---
+++ Code/func.h
@@ -0,0 +1,7 @@
+#ifndef _func_H_
+#define _func_H_
+
+void fire(float bullet_trn);
+void draw_tarrget(int h, int rot, int trn);
+
+#endif
